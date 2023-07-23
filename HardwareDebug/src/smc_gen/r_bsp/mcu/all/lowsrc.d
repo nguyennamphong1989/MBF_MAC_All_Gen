8a26cbde7cc2026c7b813173d4e461ab6370d03c
nguyennamphong1989 <nguyennamphong1989@gmail.com>
2023-07-24 01:04:48 +0700

    Remove HardwareDebug folder

--- HardwareDebug/src/smc_gen/r_bsp/mcu/all/lowsrc.d
+++
@@ -1,9 +0,0 @@
-src/smc_gen/r_bsp/mcu/all/lowsrc.obj src/smc_gen/r_bsp/mcu/all/lowsrc.d: ../src/smc_gen/r_bsp/mcu/all/lowsrc.c
-src/smc_gen/r_bsp/mcu/all/lowsrc.obj src/smc_gen/r_bsp/mcu/all/lowsrc.d: ../src/smc_gen/r_bsp/mcu/all/r_bsp_common.h
-../src/smc_gen/r_bsp/mcu/all/r_bsp_common.h:
-src/smc_gen/r_bsp/mcu/all/lowsrc.obj src/smc_gen/r_bsp/mcu/all/lowsrc.d: C:\Users\Admin\e2_studio\workspace\MAC_All_Gen\src\smc_gen\r_config\r_bsp_config.h
-C:\Users\Admin\e2_studio\workspace\MAC_All_Gen\src\smc_gen\r_config\r_bsp_config.h:
-src/smc_gen/r_bsp/mcu/all/lowsrc.obj src/smc_gen/r_bsp/mcu/all/lowsrc.d: ../src/smc_gen/r_bsp/mcu/all/lowlvl.h
-../src/smc_gen/r_bsp/mcu/all/lowlvl.h:
-src/smc_gen/r_bsp/mcu/all/lowsrc.obj src/smc_gen/r_bsp/mcu/all/lowsrc.d: ../src/smc_gen/r_bsp/mcu/all/lowsrc.h
-../src/smc_gen/r_bsp/mcu/all/lowsrc.h:
